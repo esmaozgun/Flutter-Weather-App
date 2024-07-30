e5c60146ead44eaaf02cc42c791fe00b60e55748
Esma Özgün <nuresma349@gmail.com>
2024-07-30 15:39:32 +0300

    Update home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:weatherapp/services/weather_service.dart';
+import '../services/weather_service.dart';
+import '../model/weather_model.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -8,27 +9,22 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   String? _selectedCity;
-  final List<String> _cities = ['İstanbul', 'Ankara', 'İzmir'];
-  Map<String, dynamic>? _weatherData;
+  WeatherService _weatherService = WeatherService();
+  Weather? _weatherData;
+  bool _isLoading = false;
 
-  final WeatherService _weatherService = WeatherService();
-
-  void _getData() async {
+  void _fetchWeatherData() async {
     if (_selectedCity != null) {
+      setState(() {
+        _isLoading = true;
+      });
+
       final data = await _weatherService.fetchWeather(_selectedCity!);
-      if (data != null) {
-        setState(() {
-          _weatherData = data;
-        });
-      } else {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Hava durumu verisi alınamadı')),
-        );
-      }
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Lütfen bir şehir seçin')),
-      );
+
+      setState(() {
+        _weatherData = data;
+        _isLoading = false;
+      });
     }
   }
 
@@ -36,72 +32,74 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Şehir Seçimi'),
+        title: Text('Weather App'),
       ),
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                width: 300, // Dropdown genişliği
-                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(color: Colors.grey, width: 1),
-                ),
-                child: DropdownButtonHideUnderline(
-                  child: DropdownButton<String>(
-                    hint: const Text('Şehir seçin'),
-                    value: _selectedCity,
-                    isExpanded: true,
-                    icon: const Icon(Icons.arrow_downward),
-                    iconSize: 24,
-                    elevation: 16,
-                    style: const TextStyle(color: Colors.black, fontSize: 18),
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        _selectedCity = newValue;
-                        _weatherData = null;
-                      });
-                    },
-                    items: _cities.map<DropdownMenuItem<String>>((String city) {
-                      return DropdownMenuItem<String>(
-                        value: city,
-                        child: Text(city),
-                      );
-                    }).toList(),
-                  ),
-                ),
+            children: [
+              DropdownButton<String>(
+                value: _selectedCity,
+                hint: Text('Şehir seçin'),
+                items: ['Istanbul', 'Ankara', 'Izmir'].map((String city) {
+                  return DropdownMenuItem<String>(
+                    value: city,
+                    child: Text(
+                      city,
+                      style: TextStyle(fontSize: 18.0),
+                    ),
+                  );
+                }).toList(),
+                onChanged: (newValue) {
+                  setState(() {
+                    _selectedCity = newValue;
+                  });
+                },
               ),
-              const SizedBox(height: 30),
-              SizedBox(
-                width: 250, // Buton genişliği
-                height: 50, // Buton yüksekliği
-                child: ElevatedButton(
-                  onPressed: _getData,
-                  child: const Text(
-                    'Hava Durumunu Gör',
-                    style: TextStyle(fontSize: 18),
-                  ),
-                ),
+              SizedBox(height: 40),
+              ElevatedButton(
+                onPressed: _fetchWeatherData,
+                child: Text('Hava Durumunu Gör', style: TextStyle(fontSize: 18.0)),
               ),
-              const SizedBox(height: 30),
-              _weatherData != null
+              SizedBox(height: 40),
+              _isLoading
+                  ? CircularProgressIndicator()
+                  : _weatherData != null
                   ? Column(
                 children: [
                   Text(
-                    'Sıcaklık: ${_weatherData!['main']['temp']}°C',
-                    style: TextStyle(fontSize: 20),
+                    'Sıcaklık: ${_weatherData!.temp}°C',
+                    style: TextStyle(fontSize: 20.0),
                   ),
                   Text(
-                    'Durum: ${_weatherData!['weather'][0]['description']}',
-                    style: TextStyle(fontSize: 20),
+                    'Durum: ${_weatherData!.description}',
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                  Text(
+                    'Hissedilen: ${_weatherData!.feelsLike}°C',
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                  Text(
+                    'Minimum Sıcaklık: ${_weatherData!.tempMin}°C',
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                  Text(
+                    'Maximum Sıcaklık: ${_weatherData!.tempMax}°C',
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                  Text(
+                    'Basınç: ${_weatherData!.pressure} hPa',
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                  Text(
+                    'Nem: ${_weatherData!.humidity}%',
+                    style: TextStyle(fontSize: 20.0),
                   ),
                 ],
               )
-                  : Container(),
+                  : SizedBox.shrink(),
             ],
           ),
         ),
