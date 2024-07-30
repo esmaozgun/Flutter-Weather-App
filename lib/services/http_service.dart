4e23d345b587b109d400b90d48c8e4e8dc245e52
Esma Özgün <nuresma349@gmail.com>
2024-07-30 15:41:19 +0300

    Update http_service.dart

--- lib/services/http_service.dart
+++ lib/services/http_service.dart
@@ -1,12 +1,7 @@
 import 'package:http/http.dart' as http;
-
 class HttpService {
   Future<http.Response> getRequest(String url) async {
-    try {
-      final response = await http.get(Uri.parse(url));
-      return response;
-    } catch (e) {
-      throw Exception('Failed to load data');
-    }
+    final response = await http.get(Uri.parse(url));
+    return response;
   }
 }
